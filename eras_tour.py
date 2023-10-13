8da050f90f12e526cee4a12518957f9a5d23ad6d
melisilva <55757043+melisilva@users.noreply.github.com>
2023-10-13 17:55:38 +0100

    Check if length of items changed, add new bot commands

--- eras_tour.py
+++ eras_tour.py
@@ -16,12 +16,12 @@ client = discord.Client(intents=discord.Intents.all())
 URL = "https://taylorswift.seetickets.com/resell/search"
 prev_length = 0  # Store the previous value of len(taylor_results)
 
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}')
 
-
-async def check_website():
+async def check_website(message = None):
     global prev_length
     print("holo 3")
 
@@ -33,8 +33,12 @@ async def check_website():
     channel = client.get_channel(int(channel_id))  # Convert to int and get the channel
 
     if current_length > prev_length:
-        prev_length = current_length
-        await channel.send(f"Warning: Number of results increased to {current_length}")
+        await channel.send(f"@everyone Warning: Number of results increased to {current_length}")
+    else:
+        if message is not None:
+            await channel.send("No new items, sorry :(")
+
+    prev_length = current_length
 
 @client.event
 async def on_message(message):
@@ -42,11 +46,13 @@ async def on_message(message):
     channel = client.get_channel(int(channel_id))  # Convert to int and get the channel
     if message.content == '!alive':
         await channel.send("I'm alive. There's just no new items.")
+    elif message.content == '!check':
+        await check_website(message.content)
 
 if __name__ == "__main__":
     print("holo")
 
-    cron = crontab("*/10 * * * *")  # Schedule to run every minute
+    cron = crontab("*/10 * * * *")  # Schedule to run every 10 minutes
     cron(check_website)
 
     loop = asyncio.get_event_loop()
