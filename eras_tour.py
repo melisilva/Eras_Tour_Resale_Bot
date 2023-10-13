80dc66c7cb7a117a73c49a6adae659aa5e9ca9cb
melisilva <55757043+melisilva@users.noreply.github.com>
2023-10-13 15:08:02 +0100

    Fixed major bugs and added command to bot

--- eras_tour.py
+++ eras_tour.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import discord
-from apscheduler.schedulers.background import BackgroundScheduler
-import os
+from aiocron import crontab
+import asyncio
+
+DISCORD_TOKEN= ""
+YOUR_CHANNEL_ID= ""
 
 # Discord bot token
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = DISCORD_TOKEN
 
 # Discord client
 client = discord.Client(intents=discord.Intents.all())
@@ -17,30 +20,35 @@ prev_length = 0  # Store the previous value of len(taylor_results)
 async def on_ready():
     print(f'Logged in as {client.user.name}')
 
+
 async def check_website():
     global prev_length
+    print("holo 3")
 
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "lxml")
     taylor_results = soup.find_all("ul")
     current_length = len(taylor_results)
-    channel_id = os.getenv("YOUR_CHANNEL_ID")
+    channel_id = YOUR_CHANNEL_ID
     channel = client.get_channel(int(channel_id))  # Convert to int and get the channel
 
     if current_length > prev_length:
         prev_length = current_length
         await channel.send(f"Warning: Number of results increased to {current_length}")
-    else:
-        await channel.send("Bot is working, but no new items found.")
 
 @client.event
 async def on_message(message):
-    if message.content == '!check':
-        await check_website()
+    channel_id = YOUR_CHANNEL_ID
+    channel = client.get_channel(int(channel_id))  # Convert to int and get the channel
+    if message.content == '!alive':
+        await channel.send("I'm alive. There's just no new items.")
 
-# Schedule the website check every hour
-scheduler = BackgroundScheduler()
-scheduler.add_job(check_website, 'interval', hours=1)
-scheduler.start()
+if __name__ == "__main__":
+    print("holo")
 
-client.run(TOKEN)
+    cron = crontab("*/10 * * * *")  # Schedule to run every minute
+    cron(check_website)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(client.start(TOKEN))
+    loop.run_forever()
